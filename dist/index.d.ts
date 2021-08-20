364dbf34fc06da635aab0d252b500520762aa276
Viggo Jonasson <aethezjonasson@gmail.com>
2021-08-20 20:35:23 +0200

    add new changes and build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,57 @@
-import { BuyListing, SellListing } from './types/bptf';
 export interface ParseResult {
     paint: string;
     spells: string[];
     parts: string[];
 }
+export declare type BuyListing = Listing & {
+    item: BuyOrderItem;
+    intent: 0;
+};
+export declare type SellListing = Listing & {
+    item: SellOrderItem;
+    intent: 1;
+};
+export declare type Listing = {
+    id: string;
+    steamid: string;
+    appid: number;
+    currencies: {
+        metal: number;
+        keys: number;
+    };
+    offers: number;
+    buyout: number;
+    details: string;
+    created: number;
+    bump: number;
+    intent: 1 | 0;
+    automatic: number;
+    count: number;
+    promoted: number;
+};
+export declare type OrderItem = {
+    defindex: number;
+    quality: number;
+    attributes: Attributes[];
+    name: string;
+    quantity: string;
+};
+export declare type SellOrderItem = OrderItem & {
+    id: number;
+    original_id: number;
+    level: number;
+    inventory: number;
+    origin: number;
+    style: number;
+};
+export declare type BuyOrderItem = OrderItem & {
+    'user-id': string;
+};
+export declare type Attributes = {
+    float_value?: number;
+    defindex: number;
+    value?: number | string;
+};
 export declare const parseListing: (listing: BuyListing | SellListing) => ParseResult;
 export declare const data: {
     parts: {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { BuyListing, SellListing } from './types/bptf';
-
 import parts from './lib/parts';
 import spells from './lib/spells';
 import paints from './lib/paints';
@@ -9,6 +7,64 @@ export interface ParseResult {
     spells: string[];
     parts: string[];
 }
+
+// BPTF Types from danocmx
+
+export type BuyListing = Listing & {
+    item: BuyOrderItem;
+    intent: 0;
+};
+
+export type SellListing = Listing & {
+    item: SellOrderItem;
+    intent: 1;
+};
+
+export type Listing = {
+    id: string;
+    steamid: string;
+    appid: number;
+    currencies: {
+        metal: number;
+        keys: number;
+    };
+    offers: number;
+    buyout: number;
+    details: string;
+    created: number;
+    bump: number;
+    intent: 1 | 0;
+    automatic: number;
+    count: number;
+    promoted: number;
+};
+
+export type OrderItem = {
+    defindex: number;
+    quality: number;
+    attributes: Attributes[];
+    name: string;
+    quantity: string;
+};
+
+export type SellOrderItem = OrderItem & {
+    id: number;
+    original_id: number;
+    level: number;
+    inventory: number;
+    origin: number;
+    style: number;
+};
+
+export type BuyOrderItem = OrderItem & {
+    'user-id': string;
+};
+
+export type Attributes = {
+    float_value?: number;
+    defindex: number;
+    value?: number | string;
+};
 
 export const parseListing = (
     listing: BuyListing | SellListing
